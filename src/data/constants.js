3f8ff457d093a9614c917f1c5b6cb9b4822034ec
UTKARSH SINGH VIVEK <96197640+utkarsh462255@users.noreply.github.com>
2024-07-29 14:04:38 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -206,15 +206,15 @@ export const projects = [
   {
     id: 5,
     title: "Todo Web App",
-    date: "Jun 2021",
+    date: "Jun 2024",
     description:
-      " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+      " A Simple Todo Web App made with React JS, TailwindCSS. User can add todos. It has feature to show finished work separately",
     image:
       "https://camo.githubusercontent.com/84ac6ab6f378348ef28d8184062b7e9e3511a1252ae3966eaa49e8e998f732a7/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
-    tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
+    tags: ["React Js", "TailwindCSS"],
     category: "web app",
-    github: "",
-    webapp: "",
+    github: "https://github.com/utkarsh462255/To-do-list",
+    webapp: "https://incredible-torrone-82e34c.netlify.app/",
   },
   {
     id: 7,
